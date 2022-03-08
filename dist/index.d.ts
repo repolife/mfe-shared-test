b1d48e34778a40769936fb2d0a650457b8263a57
repolife <getdavidvargas@gmail.com>
2022-03-08 10:52:48 -0600

    test url

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,7 +1,7 @@
 import React from "react";
 export declare type Imporant = React.FunctionComponent<{
-  user: string;
-  age: number;
+    user: string;
+    age: number;
 }>;
 declare const _default: true;
 export default _default;
---
+++ src/component/example.ts
@@ -0,0 +1,5 @@
+import React from "react";
+
+export type Example = React.FunctionComponent<{ ass: "thick" | "skinny" }>;
+
+export default true;
